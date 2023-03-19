5dbd44d495628f4363de563e0db0d3ad8a6025f5
ahmedsaadbadawy <125485653+ahmedsaadbadawy@users.noreply.github.com>
2023-03-19 19:54:42 +0200

    Update screen1.dart

--- my_Archives_of_flutter/Transfer_Between_Screens/screen1.dart
+++ my_Archives_of_flutter/Transfer_Between_Screens/screen1.dart
@@ -1,1 +1,42 @@
  
+import 'package:flutter/material.dart';
+import 'screen2.dart';
+
+class Screen1 extends StatelessWidget {
+  static const routName = '/my_screen1_category';
+
+  void SelectScreen(BuildContext ctx) {
+    Navigator.of(ctx).pushReplacementNamed(
+      '/x2',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    late Map<String, Object> routeArg =
+        ModalRoute.of(context)?.settings.arguments as Map<String, Object>;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen 1"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Screen1",
+              style: const TextStyle(fontSize: 30, color: Colors.teal),
+            ),
+            InkWell(
+              child: const Text(
+                "Go To Screen 2",
+                style: TextStyle(fontSize: 30, color: Colors.teal),
+              ),
+              onTap: () => SelectScreen(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
