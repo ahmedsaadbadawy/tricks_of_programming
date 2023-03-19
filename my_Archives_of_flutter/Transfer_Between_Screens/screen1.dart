1b636191ae79aac46ab5e095629949c7de5b9fef
ahmedsaadbadawy <125485653+ahmedsaadbadawy@users.noreply.github.com>
2023-03-19 20:21:15 +0200

    Update screen1.dart

--- my_Archives_of_flutter/Transfer_Between_Screens/screen1.dart
+++ my_Archives_of_flutter/Transfer_Between_Screens/screen1.dart
@@ -1,13 +1,16 @@
- 
 import 'package:flutter/material.dart';
 import 'screen2.dart';
 
 class Screen1 extends StatelessWidget {
   static const routName = '/my_screen1_category';
 
-  void SelectScreen(BuildContext ctx) {
+  void SelectScreen(BuildContext ctx, int n) {
     Navigator.of(ctx).pushReplacementNamed(
       '/x2',
+      arguments: {
+        'id': n == 1 ? 10 : 20,
+        'title': n == 1 ? "info1" : "info2",
+      },
     );
   }
 
@@ -24,7 +27,11 @@ class Screen1 extends StatelessWidget {
         child: Column(
           children: [
             Text(
-              "Screen1",
+              "${routeArg['id']}",
+              style: const TextStyle(fontSize: 30, color: Colors.teal),
+            ),
+            Text(
+              "${routeArg['title']}",
               style: const TextStyle(fontSize: 30, color: Colors.teal),
             ),
             InkWell(
@@ -32,7 +39,7 @@ class Screen1 extends StatelessWidget {
                 "Go To Screen 2",
                 style: TextStyle(fontSize: 30, color: Colors.teal),
               ),
-              onTap: () => SelectScreen(context),
+              onTap: () => SelectScreen(context, 2),
             ),
           ],
         ),
